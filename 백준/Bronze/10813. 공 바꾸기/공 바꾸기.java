31cc0b5b71122ccd663ea54aa385d411807d603c
ttttkii913 <139212932+ttttkii913@users.noreply.github.com>
2023-07-17 18:17:32 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		int temp;
+
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = i + 1;
+		for (int j = 0; j < m; j++) { // i번부터 j번 바구니
+			st = new StringTokenizer(br.readLine());
+			int I = Integer.parseInt(st.nextToken()); // 1 2 3 처럼 ijk 입력받음
+			int J = Integer.parseInt(st.nextToken());
+
+			temp = arr[I - 1];
+			arr[I - 1] = arr[J - 1];
+			arr[J - 1] = temp;
+		}
+		for (int k = 0; k < arr.length; k++)
+			sb.append(arr[k] + " ");
+		System.out.println(sb);
+	}
+}
