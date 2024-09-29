acd33fa1533817b8f02ca9af8c626d3f6f606f57
hayeong <139212932+ttttkii913@users.noreply.github.com>
2024-09-29 22:36:52 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,2 +1,7 @@
 -- 코드를 입력하세요
-SELECT
+SELECT ai.animal_id, ai.animal_type, ai.name
+FROM animal_ins ai
+JOIN animal_outs ao
+USING(animal_id)
+WHERE ai.sex_upon_intake != ao.sex_upon_outcome
+ORDER BY ai.animal_id
