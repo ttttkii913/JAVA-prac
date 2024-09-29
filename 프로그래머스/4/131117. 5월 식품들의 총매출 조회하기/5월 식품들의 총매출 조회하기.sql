1000fada20ebcf0251f60e8e9ae17dd8e3a39d9e
hayeong <139212932+ttttkii913@users.noreply.github.com>
2024-09-29 22:35:28 +0900

    Update 5월 식품들의 총매출 조회하기.sql

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,2 +1,8 @@
 -- 코드를 입력하세요
-SELECT
+SELECT product_id, product_name, SUM(p.price * o.amount) TOTAL_SALES
+FROM FOOD_PRODUCT p
+JOIN FOOD_ORDER o
+USING (product_id)
+WHERE o.produce_date LIKE "2022-05%"
+GROUP BY p.product_id
+ORDER BY total_sales DESC, product_id ASC
