a58de554ac21c1edd0be0e7c1c4ea3162d392411
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-01-28 13:39:20 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,33 +1,29 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		int res = 0;
-		// abc가 같을 때
-		if (A == B && B == C)
-			res = 10000 + A * 1000;
-		// abc가 다 다를 때
-		else if (A != B && B != C && A != C) {
-			if (A > B && A > C)
-				res = A * 100;
-			else if (B > A && B > C)
-				res = B * 100;
-			else if (C > A && C > B)
-				res = C * 100;
-		} else {
-			if (A == B)
-				res = 1000 + A * 100;
-			else if (A == C)
-				res = 1000 + A * 100;
-			else if (B == C)
-				res = 1000 + B * 100;
-		}
-		System.out.println(res);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int res = 0;
+
+        if (a == b && b == c) {
+            res = 10000 + a * 1000;
+        } else if (a == b || a == c) {
+            res = 1000 + a * 100;
+        } else if (b == c) {
+            res = 1000 + b * 100;
+        } else {
+            res = Math.max(a, Math.max(b, c)) * 100;
+        }
+
+        System.out.println(res);
+
+    }
 }
