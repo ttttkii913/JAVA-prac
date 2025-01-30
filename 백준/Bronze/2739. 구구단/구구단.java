20d3b217827d125e335a9229143a3badca16267a
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-01-30 23:30:56 +0900

    [Bronze V] Title: 구구단, Time: 132 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,18 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		for (int i =1; i <=9; i++) { 
-			System.out.println(N + " * " + i + " = " + N*i );
-		}	
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int mul = 0;
+        
+        for (int i = 1; i <= 9; i++) {
+            mul = n * i;
+            System.out.println(n + " * " + i +" = " + mul);
+        }
+    }
 }
