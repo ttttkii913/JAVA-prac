08c3253abeb8416412aea72e279ddc66090f63ba
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-02-01 23:02:15 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 120 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,33 +1,36 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		int king = 1;
-		int queen = 1;
-		int rook = 2;
-		int bishop = 2;
-		int knight = 2;
-		int pawn = 8;
+        int king = 1;
+        int queen = 1;
+        int rook = 2;
+        int bishop = 2;
+        int knight = 2;
+        int pawn = 8;
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		king = king - Integer.parseInt(st.nextToken());
-		queen = queen - Integer.parseInt(st.nextToken());
-		rook = rook - Integer.parseInt(st.nextToken());
-		bishop = bishop - Integer.parseInt(st.nextToken());
-		knight = knight - Integer.parseInt(st.nextToken());
-		pawn = pawn - Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        king -= Integer.parseInt(st.nextToken());
+        queen -= Integer.parseInt(st.nextToken());
+        rook -= Integer.parseInt(st.nextToken());
+        bishop -= Integer.parseInt(st.nextToken());
+        knight -= Integer.parseInt(st.nextToken());
+        pawn -= Integer.parseInt(st.nextToken());
 
-		sb.append(king + " ");
-		sb.append(queen + " ");
-		sb.append(rook + " ");
-		sb.append(bishop + " ");
-		sb.append(knight + " ");
-		sb.append(pawn + " ");
+        sb.append(king + " ");
+        sb.append(queen + " ");
+        sb.append(rook + " ");
+        sb.append(bishop + " ");
+        sb.append(knight + " ");
+        sb.append(pawn + " ");
 
-		System.out.println(sb);
-	}
+        System.out.println(sb);
+    }
+
 }
