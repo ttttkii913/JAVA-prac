95e0ae023bc478ce2bea72ea05b1c5b2e2e36339
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-02-02 23:30:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,17 +1,22 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= i; j++)
-				sb.append("*");
-			sb.append("\n");
-		}
-		System.out.print(sb);
-	}
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
