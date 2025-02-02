c5c171780890614f66f110b6307b46fffd968dc8
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-02-02 23:22:32 +0900

    [Bronze V] Title: A+B - 5, Time: 120 ms, Memory: 15708 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,21 +1,24 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		int sum = 0;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-		while (true) {
-			String[] arr = br.readLine().split(" ");
-			int a = Integer.parseInt(arr[0]);
-			int b = Integer.parseInt(arr[1]);
-			sum = a + b;
-			if (a == 0 && b == 0)
-				break;
-			sb.append(sum).append('\n');
-		}
-		System.out.println(sb);
-	}
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            sb.append(a + b + "\n");
+        }
+        System.out.println(sb);
+    }
 }
