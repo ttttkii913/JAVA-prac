43a81cf62e9c1a9056cfd9507be0eb287b36051e
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-02-04 18:54:35 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,12 +1,12 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
-		String str = br.readLine();
-		sb.append(str.length());
-		System.out.println(sb);
-	}
+        System.out.println(str.length());
+    }
 }
