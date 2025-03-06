17210c0c8bb34b3dfd009fe6e0b7b79e6d13491a
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-03-06 19:37:03 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 140 ms, Memory: 15820 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        sb.append("<");
+
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            for (int i = 1; i < k; i++) {
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll());
+
+            if (!queue.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+        
+        br.close();
+        System.out.print(sb);
+        
+    }
+}
