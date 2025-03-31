823e5ac707da02a5cf4a5f1af51f2e8c191fe9ab
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-04-01 02:07:29 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.05 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(String my_string, String target) {
-        if (my_string.contains(target)) {
-            return 1;
-        }
-        return 0;
+        return (my_string.contains(target) ? 1 : 0);
+            
     }
 }
