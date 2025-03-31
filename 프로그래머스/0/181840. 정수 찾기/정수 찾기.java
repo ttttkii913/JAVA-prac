28530ce87473be265561181edea165c57dd1a4d0
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-03-31 20:10:58 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
         
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] == n) {
-                answer = 1;
-                break;
-            } else {
-                answer = 0;
+        for (int i : num_list) {
+            if (i == n) {
+                return 1;
             }
         }
-        return answer;
+        return 0;
     }
 }
