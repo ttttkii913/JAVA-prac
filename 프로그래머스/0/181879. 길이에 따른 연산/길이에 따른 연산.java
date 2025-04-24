e364f5603cdcac46db372c118c07b6bbf17f6f0a
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-04-24 17:13:12 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -2,14 +2,17 @@ class Solution {
     public int solution(int[] num_list) {
         int sum = 0;
         int mul = 1;
-        
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list.length >= 11) {
-                sum += num_list[i]; 
-            } else if (num_list.length <= 10) {
-                mul *= num_list[i];
+       
+        if (num_list.length > 10) {
+            for (int num : num_list) {
+                sum += num;
+            } 
+            return sum;
+        } else {
+            for (int num : num_list) {
+                mul *= num;
             }
+            return mul;
         }
-        return (num_list.length >= 11 ? sum : mul);
     }
 }
